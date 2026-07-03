87cfba193fb5c6f1c7e59f3533b54b6fb076b468
Ambrish Kalia <117847499+Ambrish8@users.noreply.github.com>
2026-07-03 15:58:46 +0530

    Initialize model inference with YOLO and constants
    Import necessary libraries and set up constants for model inference.

--- src/model_inference.py
+++ src/model_inference.py
@@ -1,1 +1,26 @@
+import os, random, re, math, csv, xml.etree.ElementTree as ET
+from pathlib import Path
+import cv2
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from ultralytics import YOLO
+from PIL import Image
+import torch
+import easyocr
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+import Levenshtein
 
+YOLO_WEIGHTS = "/content/best.pt"
+TEST_IMG_DIR = "/content/drive/MyDrive/datas2/images"
+XML_DIR = "/content/drive/MyDrive/datas2/images"
+OUTPUT_DIR = "/content/output"
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+N_IMAGES = 300
+N_VIS = 5
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+GROUND_TRUTH_CSV = os.path.join(OUTPUT_DIR, "ground_truth.csv")
+
+print("Loading YOLO...")
+yolo = YOLO(YOLO_WEIGHTS)
